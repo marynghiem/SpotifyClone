14357b7069ed073d5872699f1f41f7d94bb29121
marynghiem <marynghiem46@gmail.com>
2023-01-06 14:20:50 -0800

    created a use effect to get the token from the url and save that info in state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./Login";
 import { getTokenFromUrl } from "./spotify";
 
 function App() {
+  const [token, setToken] = useState(null);
+
   useEffect(() => {
-    const token = getTokenFromUrl();
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+
     console.log("I HAVE A TOKEN:", token);
   }, []);
 
-  return (
-    <div className="app">
-      {/* Spotify Logo*/}
-      {/* Login with spotify button*/}
-      <Login />
-    </div>
-  );
+  return <div className="app">{token ? <h1>I am logged in</h1> : <Login />}</div>;
 }
 
 export default App;
