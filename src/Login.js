b9d08725424c88af89838b208b09c03675ee1696
marynghiem <marynghiem46@gmail.com>
2023-01-06 11:35:15 -0800

    styled login component

--- src/Login.js
+++ src/Login.js
@@ -5,7 +5,7 @@ const Login = () => {
   return (
     <div className="login">
       <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="" />
-      <button>LOGIN WITH SPOTIFY</button>
+      <a>LOGIN WITH SPOTIFY</a>
     </div>
   );
 };
