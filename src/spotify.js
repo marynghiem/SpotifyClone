2dec597582c659c8e61504f924b737d9abc0e547
marynghiem <marynghiem46@gmail.com>
2023-01-06 12:35:35 -0800

    created login url for link in login component

---
+++ src/spotify.js
@@ -0,0 +1,19 @@
+//https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
+
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+const redirectUri = "http://localhost:3000/";
+
+const clientId = "e79a1bb54a7440a7b9c04367d8efb8d9";
+
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
