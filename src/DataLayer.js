4dd41c0a629bb001818b0a7c9f9c978544c28e08
marynghiem <marynghiem46@gmail.com>
2023-01-09 16:09:22 -0800

    import usedatalayer inside app.js

--- src/DataLayer.js
+++ src/DataLayer.js
@@ -4,3 +4,5 @@ export const DataLayerContext = createContext();
 export const DataLayer = ({ initialState, reducer, children }) => (
   <DataLayerContext.Provider value={useReducer(reducer, initialState)}>{children}</DataLayerContext.Provider>
 );
+
+export const useDataLayerValue = () => useContext(DataLayerContext);
