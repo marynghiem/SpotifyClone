f637ff327caadd63b7c454219e956a6fd2135206
marynghiem <marynghiem46@gmail.com>
2023-01-10 16:44:55 -0800

    creaated sidebar component

--- src/Player.js
+++ src/Player.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./Player.css";
 
 const Player = ({ spotify }) => {
-  return <div>Welcome to Spotify</div>;
+  return (
+    <div className="player">
+      <div className="player_body"></div>
+    </div>
+  );
 };
 
 export default Player;
---
+++ src/Sidebar.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Sidebar.css";
+
+const Sidebar = () => {};
+
+export default Sidebar;
