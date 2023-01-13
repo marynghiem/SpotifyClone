8ea14399dc1e599b0af726211aefc39e6b8a66c7
marynghiem <marynghiem46@gmail.com>
2023-01-13 12:16:04 -0800

    installed correct material ui and styled sidebar title

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -16,6 +16,11 @@ const Sidebar = () => {
       <SidebarOption title="Home" Icon={HomeIcon} />
       <SidebarOption title="Search" Icon={SearchIcon} />
       <SidebarOption title="Your Library" Icon={LibraryMusicIcon} />
+      <br />
+      <strong className="sidebar_title">PLAYLISTS</strong>
+      <hr />
+      <SidebarOption title="Hip Hop" />
+      <SidebarOption title="Rock" />
     </div>
   );
 };
