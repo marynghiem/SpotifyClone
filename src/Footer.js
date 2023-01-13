d796db890e8d197fc4e546783ab3342cb6e8a31b
marynghiem <marynghiem46@gmail.com>
2023-01-13 15:46:14 -0800

    styled the center div in footer component

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,10 @@
 import React from "react";
 import "./Footer.css";
+import PlayCircleOutlineIcon from "@mui/icons-material/PlayCircleOutline";
+import SkipNextIcon from "@mui/icons-material/SkipNext";
+import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
+import ShuffleIcon from "@mui/icons-material/Shuffle";
+import RepeatIcon from "@mui/icons-material/Repeat";
 
 const Footer = () => {
   return (
@@ -8,6 +13,11 @@ const Footer = () => {
         <p>Album and song details</p>
       </div>
       <div className="footer_center">
+        <ShuffleIcon className="footer_green" />
+        <SkipPreviousIcon className="footer_icon" />
+        <PlayCircleOutlineIcon fontSize="large" className="footer_icon" />
+        <SkipNextIcon className="footer_icon" />
+        <RepeatIcon className="footer_green" />
         <p>Player controls</p>
       </div>
       <div className="footer_right">
