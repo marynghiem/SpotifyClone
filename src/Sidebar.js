fd29a593a4e9ee1b7bf142f7155159218f4ecdf9
marynghiem <marynghiem46@gmail.com>
2023-01-13 10:52:55 -0800

    styled body footer and sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,7 +2,15 @@ import React from "react";
 import "./Sidebar.css";
 
 const Sidebar = () => {
-  return <div className="sidebar"></div>;
+  return (
+    <div className="sidebar">
+      <img
+        className="sidebar_logo"
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt=""
+      />
+    </div>
+  );
 };
 
 export default Sidebar;
