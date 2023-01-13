7b836e3ac833d73ebd6846e97daed93ed18e0bd4
marynghiem <marynghiem46@gmail.com>
2023-01-13 11:29:09 -0800

    installed material ui and imported the icons

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "./Sidebar.css";
+import SidebarOption from "./SidebarOption";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import LibraryMusicIcon from "@mui/icons-material/LibraryMusic";
 
 const Sidebar = () => {
   return (
@@ -9,6 +13,9 @@ const Sidebar = () => {
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
         alt=""
       />
+      <SidebarOption title="Home" />
+      <SidebarOption title="Search" />
+      <SidebarOption title="Your Library" />
     </div>
   );
 };
--- src/SidebarOption.js
+++ src/SidebarOption.js
@@ -1,5 +1,11 @@
 import React from "react";
 import "./SidebarOption.css";
 
-const SidebarOption = () => {};
+const SidebarOption = ({ title, Icon }) => {
+  return (
+    <div className="sidebar_option">
+      <p>{title}</p>
+    </div>
+  );
+};
 export default SidebarOption;
