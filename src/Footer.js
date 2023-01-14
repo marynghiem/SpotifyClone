a4c1fd0594a1f7b7793f40dbf984672c60161deb
marynghiem <marynghiem46@gmail.com>
2023-01-13 16:13:59 -0800

    added grids and icons inside footer right container

--- src/Footer.js
+++ src/Footer.js
@@ -5,6 +5,9 @@ import SkipNextIcon from "@mui/icons-material/SkipNext";
 import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
 import ShuffleIcon from "@mui/icons-material/Shuffle";
 import RepeatIcon from "@mui/icons-material/Repeat";
+import { Grid, Slider } from "@mui/material";
+import PlaylistPlayIcon from "@mui/icons-material/PlaylistPlay";
+import VolumeDownIcon from "@mui/icons-material/VolumeDown";
 
 const Footer = () => {
   return (
@@ -18,10 +21,19 @@ const Footer = () => {
         <PlayCircleOutlineIcon fontSize="large" className="footer_icon" />
         <SkipNextIcon className="footer_icon" />
         <RepeatIcon className="footer_green" />
-        <p>Player controls</p>
       </div>
       <div className="footer_right">
-        <p>Volume controls</p>
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider aria-labelledby="continuous-slider" />
+          </Grid>
+        </Grid>
       </div>
     </div>
   );
