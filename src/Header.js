6f3f340c9de4f8287bba91c8d483e114adee3169
marynghiem <marynghiem46@gmail.com>
2023-01-20 12:03:36 -0800

    content for right container of header and styled header with css

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
+import { Avatar } from "@mui/material";
 
 const Header = ({ spotify }) => {
   return (
@@ -9,7 +10,10 @@ const Header = ({ spotify }) => {
         <SearchIcon />
         <input placeholder="Search for Artists, Songs, or Podcasts " type="text" />
       </div>
-      <div className="header_right"></div>
+      <div className="header_right">
+        <Avatar src="" alt="MN" />
+        <h4>Mary Nghiem</h4>
+      </div>
     </div>
   );
 };
