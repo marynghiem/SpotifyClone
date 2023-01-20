e075bad7bbd1b421ced7b56e37b75c007524bb46
marynghiem <marynghiem46@gmail.com>
2023-01-20 11:40:32 -0800

    content for left container of header component

--- src/Body.js
+++ src/Body.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./Body.css";
+import Header from "./Header";
 
-const Body = () => {
-  return <div className="body"></div>;
+const Body = ({ spotify }) => {
+  return (
+    <div className="body">
+      <Header spotify={spotify} />
+    </div>
+  );
 };
 
 export default Body;
--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,17 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
 
-const Header = () => {};
+const Header = ({ spotify }) => {
+  return (
+    <div className="header">
+      <div className="header_left">
+        <SearchIcon />
+        <input placeholder="Search for Artists, Songs, or Podcasts " type="text" />
+      </div>
+      <div className="header_right"></div>
+    </div>
+  );
+};
 
 export default Header;
