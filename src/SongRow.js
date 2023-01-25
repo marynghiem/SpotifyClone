6bee7879dad30317f0e8355fc4e5d0cd48a77b40
marynghiem <marynghiem46@gmail.com>
2023-01-24 19:57:21 -0800

    finished adding some content in song row component

--- src/SongRow.js
+++ src/SongRow.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./SongRow.css";
 
-const SongRow = () => {
-  return;
+const SongRow = ({ track = "test" }) => {
+  return (
+    <div className="song_row">
+      <img className="song_row_album" src={track.album.images[0].url} />
+      <div className="song_row_info">
+        <h1>{track.name}</h1>
+        <p>
+          {track.artists.map((artist) => artist.name).join(",")}
+          {track.album.name}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default SongRow;
